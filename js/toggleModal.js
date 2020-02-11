fb3698f8c776ce7940047c79f1e7e8875a2565c2
claudiabdm <claudiabdm@hotmail.es>
2020-02-11 12:16:36 +0100

    svg sprite added

--- js/toggleModal.js
+++ js/toggleModal.js
@@ -1,7 +1,6 @@
-console.log($('#addMemBtn'));
-
 $('#addMemBtn, .modal__close').on('click', function(e){
 	$('.modal').toggleClass('modal--show')
+	$('.modal__fade').toggleClass('modal__fade--show')
 	$('.modal__container').toggleClass('modal__container--show')
 }
 )
