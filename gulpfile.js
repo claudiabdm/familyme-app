f821ed7f8af702eccce913661f1c629875449b7b
claudiabdm <claudiabdm@hotmail.es>
2020-02-15 22:52:13 +0100

    updated using sass and gulp compiler

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Node Sass se utilizará de forma predeterminada,
+// aún así se recomienda que se configure explícitamente
+// para mantener una compatibilidad futura en caso de que
+// cambie el valor predeterminado.
+sass.compiler = require('node-sass');
+ 
+
+// Compilar los ficheros SCSS y enviar el CSS resutlante
+// en la carpeta /assets/css
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('../../sass/sourcemap/'))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+// Vigilar si hay algún cambio en los SCSS
+gulp.task('default', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
+
--- js/toggleModal.js
+++ js/toggleModal.js
@@ -1,6 +1,19 @@
-$('#addMemBtn, .modal__close').on('click', function(e){
-	$('.modal').toggleClass('modal--show')
-	$('.modal__fade').toggleClass('modal__fade--show')
-	$('.modal__container').toggleClass('modal__container--show')
+function showModal(modalElem) {
+		$(modalElem).toggleClass('modal--show');
+		$('.modal__fade').toggleClass('modal__fade--show');
+		$('.modal__container').toggleClass('modal__container--show');
 }
-)
+
+$('#newMemberBtn').click(function(e){
+	const modalElem = $('#newMemberModal');
+	showModal(modalElem);
+})
+
+$('#settingsBtn').click(function(e){
+	const modalElem = $('#settingsModal');
+	showModal(modalElem);
+})
+
+$('.modal__close').click(() => {
+	$('.modal').removeClass('modal--show');
+})
