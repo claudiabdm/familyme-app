1200dc1bdaa4aa1cb021fff9a9897a9a045f982d
claudiabdm <claudiabdm@hotmail.es>
2020-02-18 16:53:51 +0100

    add edit photo and add invitee modals

--- js/toggleModal.js
+++ js/toggleModal.js
@@ -1,37 +1,48 @@
 function showModal(modalElem) {
-		// $('.container').toggleClass('container--hidden');
-		$(modalElem).toggleClass('modal--show');
-		$('.modal__fade').toggleClass('modal__fade--show');
-		$('.modal__container').toggleClass('modal__container--show');
+  // $('.container').toggleClass('container--hidden');
+  $(modalElem).toggleClass('modal--show');
+  $('.modal__fade').toggleClass('modal__fade--show');
+  $('.modal__container').toggleClass('modal__container--show');
 }
 
-//--- BUTTONS LISTENERS ---//
-$('#newMemberBtn').click(function(){
-	const modalElem = $('#newMemberModal');
-	showModal(modalElem);
+// --- BUTTONS LISTENERS --- //
+
+$('#newMemberBtn').click(function () {
+  const modalElem = $('#newMemberModal');
+  showModal(modalElem);
 })
 
-$('#settingsBtn').click(function(){
-	const modalElem = $('#settingsModal');
-	showModal(modalElem);
+$('#settingsBtn').click(function () {
+  const modalElem = $('#settingsModal');
+  showModal(modalElem);
 })
 
-$('#createBtn').click(function(){
-	const modalElem = $('#createModal');
-	showModal(modalElem);
+$('#imgBtn').click(function () {
+  const modalElem = $('#editPhotoModal');
+  showModal(modalElem);
 })
 
-$('#joinBtn').click(function(){
-	const modalElem = $('#joinModal');
-	showModal(modalElem);
+$('#createBtn').click(function () {
+  const modalElem = $('#createModal');
+  showModal(modalElem);
 })
 
-$('#newEventBtn').click(function(){
-	const modalElem = $('#newEventModal');
-	showModal(modalElem);
+$('#joinBtn').click(function () {
+  const modalElem = $('#joinModal');
+  showModal(modalElem);
+})
+
+$('#newEventBtn').click(function () {
+  const modalElem = $('#newEventModal');
+  showModal(modalElem);
+})
+
+$('#addInviteeBtn').click(function () {
+  const modalElem = $('#addInviteeModal');
+  showModal(modalElem);
 })
 
 $('.modal__close').click(() => {
-	$('.modal').removeClass('modal--show');
-	// $('.container').removeClass('container--hidden');
+  $('.modal').removeClass('modal--show');
+  // $('.container').removeClass('container--hidden');
 })
