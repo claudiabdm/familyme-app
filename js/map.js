c6b2bff1ac0b366205d4125f70e98fa5cc291318
claudiabdm <claudiabdm@hotmail.es>
2020-02-21 19:57:58 +0100

    map added to locator.html

---
+++ js/map.js
@@ -0,0 +1,47 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('googleMap'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644,
+    },
+    zoom: 10,
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      // marker
+      let marker = new google.maps.Marker({
+        position: pos,
+        map,
+        title: 'Your location',
+      });
+      // center current position in map
+      map.setCenter(pos);
+    }, () => {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
