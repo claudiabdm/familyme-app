77537677fe6426153b69ffb420b4d21d52253f65
Claudia <claudiabdm@hotmail.es>
2020-03-14 18:47:09 +0100

    Add page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -7,7 +8,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   },
   { path: '', redirectTo: '/pages/login', pathMatch: 'full' },
-  // { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,11 +11,12 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ListComponent } from './list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, CalendarComponent, LoginComponent, SignupComponent, PagesComponent, ListComponent,],
+  declarations: [HomeComponent, CalendarComponent, LoginComponent, SignupComponent, PagesComponent, ListComponent, PageNotFoundComponent,],
   imports: [
     CommonModule,
     SharedModule,
