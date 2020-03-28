052b8f1864f51afb2707dda47c70aa791c86837c
Claudia <claudiabdm@hotmail.es>
2020-03-29 00:08:21 +0100

    Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/personal-project-routing'),
+      dir: require('path').join(__dirname, './coverage/personal-project'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
