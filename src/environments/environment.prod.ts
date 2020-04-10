0fafd29e9f00a664c60015bfaf924afa20f91b7f
Claudia <claudiabdm@hotmail.es>
2020-04-10 21:20:07 +0200

    Change DB URL in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/v1/',
+  apiUrl: process.env.MONGODB_PROD,
   mapBoxToken: 'pk.eyJ1IjoiY2xhdWRpYWJkbSIsImEiOiJjazd6NmFmZmwwMm9rM21wYjh6NjdpbmRmIn0.6q9bu7QHSFgC9NS3BEoHUQ'
 };
