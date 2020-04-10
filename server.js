a5479c3e7fac238dc0c568b002d04edb8c6112cd
Claudia <claudiabdm@hotmail.es>
2020-04-10 21:54:40 +0200

    Delete comment

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/personal-project/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
