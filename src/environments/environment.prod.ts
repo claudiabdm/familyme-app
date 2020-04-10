3d92115a11b810787953fb5f127a3554da91498c
Claudia <claudiabdm@hotmail.es>
2020-04-10 22:13:20 +0200

    Update api url in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.MONGODB_PROD,
+  apiUrl: 'https://familyme-api.herokuapp.com/api/v1',
   mapBoxToken: 'pk.eyJ1IjoiY2xhdWRpYWJkbSIsImEiOiJjazd6NmFmZmwwMm9rM21wYjh6NjdpbmRmIn0.6q9bu7QHSFgC9NS3BEoHUQ'
 };
