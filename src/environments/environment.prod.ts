553ca2cbad1f0caceb43d5a486111215af232bc4
Claudia <claudiabdm@hotmail.es>
2020-04-20 22:39:06 +0200

    Add socket io connection to envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  SOCKET_ENDPOINT: 'http://familyme-api.herokuapp.com/',
   apiUrl: 'https://familyme-api.herokuapp.com/api/v1/',
   mapBoxToken: 'pk.eyJ1IjoiY2xhdWRpYWJkbSIsImEiOiJjazd6NmFmZmwwMm9rM21wYjh6NjdpbmRmIn0.6q9bu7QHSFgC9NS3BEoHUQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
+  SOCKET_ENDPOINT: 'http://localhost:3000/',
   apiUrl: 'http://localhost:3000/api/v1/',
   mapBoxToken: 'pk.eyJ1IjoiY2xhdWRpYWJkbSIsImEiOiJjazd6NmFmZmwwMm9rM21wYjh6NjdpbmRmIn0.6q9bu7QHSFgC9NS3BEoHUQ',
-  SOCKET_ENDPOINT: 'http://localhost:3000/',
 };
 
 /*
