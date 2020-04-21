a9a4dd11b7431dc4e1a6937d31d291a049ba4d88
Claudia <claudiabdm@hotmail.es>
2020-04-21 13:32:39 +0200

    Add shopping notification

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Button } from 'src/app/models/button';
 import { DataService } from 'src/app/services/data.service';
 import { ModalComponent } from '../modal/modal.component';
+import { SocketioService } from 'src/app/services/socketio.service';
+import { Message } from 'src/app/models/message';
 
 @Component({
   selector: 'app-header',
@@ -14,15 +16,31 @@ export class HeaderComponent implements OnInit {
   @Input() targetModal: ModalComponent;
   @Input() switchVisible: boolean = false;
 
+  isShopping: boolean = false;
+
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private socketService: SocketioService,
   ) { }
 
   ngOnInit(): void {
+
   }
 
   get userList() {
     return this.dataService.userList;
   }
 
-}
+  onChecked(isShopping: boolean) {
+    this.socketService.setupSocketConnection();
+    this.isShopping = !isShopping;
+    let text = this.isShopping ? "I'm shopping" : "I've finished shopping";
+    const newMsg: Message = {
+      addedBy: this.dataService.user._id,
+      text: text,
+      createdAt: new Date(Date.now()),
+    } as Message;
+    this.socketService.sendMessage(newMsg);
+  }
+
+  }
