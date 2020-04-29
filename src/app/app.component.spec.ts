6c43f76ec8fe081215e9134e1515b2bbd043c36c
Claudia <claudiabdm@hotmail.es>
2020-04-29 11:27:52 +0200

    Add test for sort service and remove some unused files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('personal-project');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-project app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('personal-project app is running!');
+  // });
 });
--- src/app/services/sort.service.spec.ts
+++ src/app/services/sort.service.spec.ts
@@ -1,20 +1,78 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SortService } from './sort.service';
+import { Product } from '../models/product';
 
 describe('SortService', () => {
   let service: SortService;
+  let shoppingList;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SortService);
+    shoppingList = [
+      {
+        name: 'Milk',
+        done: false
+      },
+      {
+        name: 'Bread',
+        done: true
+      },
+      {
+        name: 'Carrot',
+        done: false
+      }
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should order alphabetically', () => {
+  it('should order A to Z', () => {
+    shoppingList = [
+      {
+        name: 'Milk'
+      },
+      {
+        name: 'Bread'
+      },
+      {
+        name: 'Carrot'
+      }
+    ];
 
+    expect(shoppingList.sort(service.sortAtoZ)).toEqual([
+      {
+        name: 'Bread'
+      },
+      {
+        name: 'Carrot'
+      },
+      {
+        name: 'Milk'
+      }
+    ])
   })
+
+  it('should order by Done', () => {
+
+
+    expect(shoppingList.sort(service.sortByDone)).toEqual([
+      {
+        name: 'Bread',
+        done: true
+      },
+      {
+        name: 'Milk',
+        done: false
+      },
+      {
+        name: 'Carrot',
+        done: false
+      },
+    ])
+  })
+
 });
