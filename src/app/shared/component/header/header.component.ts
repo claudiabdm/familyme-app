244493b61da71d29df185f0c2f2f482c9c251274
Claudia <claudiabdm@hotmail.es>
2020-05-20 18:17:21 +0200

    Update header component to new data system

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -5,6 +5,8 @@ import { ModalComponent } from '../modal/modal.component';
 import { SocketioService } from 'src/app/services/socketio.service';
 import { Message } from 'src/app/shared/models/message';
 import { User } from 'src/app/shared/models/user';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-header',
@@ -17,25 +19,24 @@ export class HeaderComponent implements OnInit {
   @Input() targetModal: ModalComponent;
   @Input() switchVisible: boolean = false;
 
+  userList: Observable<User[]>;
+  isShopping: Observable<boolean>;
+
   constructor(
     private dataService: DataService,
     private socketService: SocketioService,
   ) { }
 
   ngOnInit(): void {
-  }
-
-  get userList(): User[] {
-    return this.dataService.userList;
-  }
-
-  get isShopping(): boolean {
-    return this.dataService.user.isShopping;
+    this.userList = this.dataService.membersData$;
+    this.isShopping = this.dataService.userData$.pipe(map(user => user.isShopping));
   }
 
   onChecked() {
-    this.dataService.user.isShopping = !this.isShopping;
-    const text = this.dataService.user.isShopping ? "I'm shopping" : "I've finished shopping";
+    const user = this.dataService.getUser();
+    user.isShopping = !user.isShopping;
+    const text = user.isShopping ? "I'm shopping" : "I've finished shopping";
+    this.dataService.setUser(user);
     this.socketService.sendMessage(text);
   }
 
