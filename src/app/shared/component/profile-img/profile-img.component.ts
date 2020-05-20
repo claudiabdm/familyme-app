4c139b0dae43e9eea004d118337d1e9a57f3330f
Claudia <claudiabdm@hotmail.es>
2020-05-20 15:13:54 +0200

    Update to use User behavior subject

--- src/app/shared/component/profile-img/profile-img.component.ts
+++ src/app/shared/component/profile-img/profile-img.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 import { User } from 'src/app/shared/models/user';
 import { ModalService } from 'src/app/services/modal.service';
 import { DataService } from 'src/app/services/data.service';
 import { ImageProcessorService } from 'src/app/shared/component/profile-img/image-processor.service';
 import { UsersService } from 'src/app/services/users.service';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, take } from 'rxjs/operators';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ModalComponent } from '../modal/modal.component';
 
@@ -17,28 +17,23 @@ import { ModalComponent } from '../modal/modal.component';
 })
 export class ProfileImgComponent implements OnInit, OnDestroy {
 
-
   @Input() imageUrl: string | ArrayBuffer;
   @Output() update = new EventEmitter();
 
-  public isDisabled: boolean = true;
-  public defaultImg = '../../../../assets/img/profile-photo-round.svg';
+  user: Observable<User>;
+  isDisabled: boolean = true;
+  defaultImg = '../../../../assets/img/profile-photo-round.svg';
 
   private ngUnsubscribe$ = new Subject<void>();
 
   constructor(
-    private dataService: DataService,
     private usersService: UsersService,
     private modalService: ModalService,
     private imageProcessor: ImageProcessorService,
     private spinner: NgxSpinnerService, ) { }
 
   ngOnInit(): void {
-
-  }
-
-  get user(): User {
-    return this.dataService.user;
+    this.user = this.usersService.userData$;
   }
 
   toggleModal(targetModal: ModalComponent): void {
@@ -59,8 +54,9 @@ export class ProfileImgComponent implements OnInit, OnDestroy {
   }
 
   onUpdatePhoto(file: string | ArrayBuffer) {
-    this.dataService.user.avatar = file;
-    this.usersService.updateUser(this.dataService.user).pipe(takeUntil(this.ngUnsubscribe$)).subscribe();
+    const userToBeUpdated = this.usersService.userDataSource.getValue();
+    userToBeUpdated.avatar = file;
+    this.usersService.updateUserData(userToBeUpdated).pipe(take(1)).subscribe();
     this.update.emit();
   }
 
