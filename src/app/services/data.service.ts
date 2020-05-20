e4019472cd8081270fb1aa8d62b7cad63c0bf6af
Claudia <claudiabdm@hotmail.es>
2020-05-20 16:33:10 +0200

    Change data syste to use Behavior Subjects and Observables

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,64 +1,46 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Group } from '../shared/models/group';
 import { User } from '../shared/models/user';
-import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  user: User;
-  userGroup: Group;
-  userList: User[];
+  private _userDataSource: BehaviorSubject<User> = new BehaviorSubject(null);
+  private _membersDataSource: BehaviorSubject<User[]> = new BehaviorSubject(null);
+  private _groupDataSource: BehaviorSubject<Group>  = new BehaviorSubject(null);
+  userData$: Observable<User> = this._userDataSource.asObservable();
+  membersData$: Observable<User[]> = this._membersDataSource.asObservable();
+  groupData$: Observable<Group> = this._groupDataSource.asObservable();
 
-  constructor(
-    @Inject(LOCAL_STORAGE) private storage: StorageService,
-  ) { }
+  constructor() { }
 
-  setLocalStorage(): void {
-    if (!this.storage.get('user')) {
-      this.storage.set('user', this.user);
-      this.storage.set('userGroup', this.userGroup);
-      this.storage.set('userList', this.userList);
-    } else {
-      this.user = this.storage.get('user');
-      this.storage.set('user', this.user);
-      this.userGroup = this.storage.get('userGroup');
-      this.storage.set('userGroup', this.userGroup);
-      this.userList = this.storage.get('userList');
-      this.storage.set('userList', this.userList);
-    }
+
+  getUser(): User {
+    return this._userDataSource.getValue();
   }
 
-  setData(user: User, group: Group, users: User[]): void {
-    this.user = user;
-    this.userGroup = group;
-    this.userList = users;
-    this.setLocalStorage();
+  setUser(user: User) {
+    return this._userDataSource.next(user);
   }
 
-  updateUserData(user: User): void {
-    const idx = this.userList.findIndex(data => data._id === user._id);
-    if (idx > -1) {
-      this.user = user;
-      this.userList.splice(idx, 1, user);
-      this.updateLocalStorage('user', this.user);
-      this.updateUserList(this.userList);
-    }
+  getMembers(): User[] {
+    return this._membersDataSource.getValue();
   }
 
-  updateUserList(users: User[]): void{
-    this.userList = users;
-    this.updateLocalStorage('userList', this.userList);
+  setMembers(members: User[]) {
+    return this._membersDataSource.next(members);
   }
 
-  updateUserGroup(usersGroup: Group): void{
-    this.userGroup = usersGroup;
-    this.updateLocalStorage('userGroup', this.userGroup);
+
+  getGroup(): Group {
+    return this._groupDataSource.getValue();
   }
 
-  private updateLocalStorage(item, value): void {
-    this.storage.set(item, value);
+  setGroup(group: Group) {
+    return this._groupDataSource.next(group);
   }
+
 }
